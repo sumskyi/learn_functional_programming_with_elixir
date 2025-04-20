f38304af1b2dedf80c7dd6477e9cac06d466b815
Vladyslav Sumskyi <sumskyi@gmail.com>
2025-04-20 15:42:22 +0300

    my_turn/bob.ex doc

--- 1.work_with_functions/lib/my_turn/bob.ex
+++ 1.work_with_functions/lib/my_turn/bob.ex
@@ -1,4 +1,9 @@
 defmodule MyTurn.Bob do
+  @moduledoc """
+  Bob has traveled 200 km in four hours. Using variables, print a message
+  showing his travel distance, time, and average velocity.
+  """
+
   @distance 200
   @time 4
 
